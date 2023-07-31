eee89a2b22fae56804daeb619080573ba875d103
Gabriel Aquino <aquino.gabriel@outlook.com.br>
2023-07-31 19:20:24 -0300

    feat: first route implements test

---
+++ src/modules/base/controller/hello.controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import { controller, httpGet } from "inversify-express-utils";
+import { PathRoutes } from "../../../enum/path-routes";
+
+@controller(PathRoutes.HELLO_WORLD)
+export class HelloController {
+    @httpGet(PathRoutes.DEFAULT)
+    public index(req: Request, res: Response) {
+        res.send("Hello, world");
+    }
+}
