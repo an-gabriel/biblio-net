7824d01b450cef49b12e464e842a94f525c1f145
Gabriel Aquino <aquino.gabriel@outlook.com.br>
2023-07-31 19:17:46 -0300

    feat: add connect db

--- src/common/app.ts
+++ src/common/app.ts
@@ -2,23 +2,26 @@ import 'dotenv/config'
 require('dotenv').config();
 import express from "express";
 
+import "../database.connect"
+
 import { InversifyExpressServer } from "inversify-express-utils";
-import { DiContainer } from "../di.container";
+import cookieParser from 'cookie-parser'
+
 import logger from "./logger";
+import { DiContainer } from "../di.container";
 import { AuthMiddleware } from '../middleware.ts/auth.middleware';
 
 const container = DiContainer.getContainer();
 const server = new InversifyExpressServer(container);
 
 server.setConfig((app) => {
-    app.use(express.json());
 
     const authMiddleware = new AuthMiddleware();
     app.use((req, res, next) => {
         logger.info(req.url);
-
         next()
     }, authMiddleware.validateToken.bind(authMiddleware))
+    app.use(express.json());
 });
 
 const app = server.build();
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,5 @@
 import "reflect-metadata";
-
-import container from "./inversify.container";
-import { HelloController } from "./controllers/hello.controller";
 import app from './common/app'
-
-container.bind<HelloController>(HelloController).toSelf();
 
 app.listen(3000, () => {
     console.log("Servidor ouvindo na porta 3000...");
